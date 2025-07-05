e9726f7c029f086c671e736b0b369e3e7327c44f
temp <144443340+aamir-0@users.noreply.github.com>
2025-07-05 18:33:22 +0530

    dark mode added

---
+++ docs/to_do.js
@@ -0,0 +1,63 @@
+const todolist=[
+        ];
+       // const tododate=[];
+
+        document.querySelector('.js_input').addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    addfun();
+  }
+});//to take input by pressing the enter key
+
+   function renderhtml() {
+    const dis_obj = document.querySelector(".display_list");
+    let todohtml = [];
+
+  
+
+    for (let i = 0; i < todolist.length; i++) {
+        todohtml.push(
+            `<div>${todolist[i].name}</div>
+             <div> ${ todolist[i].date ? todolist[i].date : ''}</div>
+             <button class="del_btn" onclick="del(${i})">Delete</button>`
+        );
+    }
+    dis_obj.innerHTML = todohtml.join('');
+}
+
+function del(i) {
+    todolist.splice(i, 1);
+   // tododate.splice(i, 1);
+    renderhtml();
+}
+
+function addfun() {
+    const temp = document.querySelector(".js_input");
+    const name = temp.value;
+    const date_obj = document.querySelector(".js_date");
+    const date = date_obj.value;
+
+    if(name===''){
+      return null;
+    }
+
+    
+//creating an obj then pushing it in the array bcuz our is made of object
+    todolist.push({
+      name,
+      date
+      /*or
+      name:name
+      date:date
+      //since the variable name is same as property or arttributs of obj we can use above shorthand
+      */
+    });
+    
+
+    temp.value = '';
+    date_obj.value = '';
+
+    renderhtml();
+}
+
+
+
