bb4f075007c738891f2ca1dd9a24a26e5f3d0581
temp <144443340+aamir-0@users.noreply.github.com>
2025-07-07 00:45:57 +0530

    currency convertion funt added and radio button fixed

--- amazon-project/scripts/checkout.js
+++ amazon-project/scripts/checkout.js
@@ -1,6 +1,7 @@
 import {cart, addtocart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
+let cartsummeryHTML;
 cart.forEach((cartItem) => {
    const productId=cartItem.productId;
 
@@ -11,7 +12,9 @@ cart.forEach((cartItem) => {
         matchingProduct=product;
     }
    });
+   
    console.log(matchingProduct);
+   cartsummeryHTML+=
 
     `<div class="cart-item-container">
             <div class="delivery-date">
@@ -24,14 +27,14 @@ cart.forEach((cartItem) => {
 
               <div class="cart-item-details">
                 <div class="product-name">
-                  Black and Gray Athletic Cotton Socks - 6 Pairs
+                  ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $10.90
+                  ₹${matchingProduct.priceCents}
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">2</span>
+                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span>
                   <span class="update-quantity-link link-primary">
                     Update
@@ -88,5 +91,6 @@ cart.forEach((cartItem) => {
               </div>
             </div>
           </div>
-    `
-})
+    `;
+});
+document.querySelector('.order-summary').innerHTML=cartsummeryHTML;
