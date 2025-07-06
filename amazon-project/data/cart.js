1ea60b8b2596be1b5115392f9890cc4cab19d6a6
temp <144443340+aamir-0@users.noreply.github.com>
2025-07-06 22:45:00 +0530

    html genration added for checkout page

--- amazon-project/data/cart.js
+++ amazon-project/data/cart.js
@@ -1,6 +1,14 @@
-export const cart=[];
+export const cart=[{
+  productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  quantity:2
+},
+{
+  productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  quantity:1
+}
+];
 
-function addtocart(pname){
+export function addtocart(pname){
       let itemmatch;
      cart.forEach((item)=>{
       if(pname===item.productId){
